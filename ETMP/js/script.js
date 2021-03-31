33de858684564f9f54cd8c9f34faa0781fe4e4f8
KocekWong <34600547+KocekWong@users.noreply.github.com>
2021-03-31 13:30:29 +0800

    Create folder
    Add directory to images, CSS and javaSciprt with commented .css and .js file

---
+++ ETMP/js/script.js
@@ -0,0 +1,7 @@
+/* Filename: script.js
+   Target html: 
+   Purpose : 
+   author : Chin Jing Jie, George Can Tze Hui, Liew Woun Kai, Nicholas Lim Tun Yang
+   Date written: 31/03/2021
+   Revisions: 31/03/2021
+*/

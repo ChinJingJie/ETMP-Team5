eacc24e770992a763c1945e8b5c8b0a1cb09b8c7
CHIN JING JIE <kocekwong@gmail.com>
2021-04-05 20:15:21 +0800

    build navigation, create user profile ui, modify css and js file

---
+++ ETMP/profile.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include "header.php";?>
+<body>
+    <header>
+        <?php include "navigation.php";?>
+    </header>
+    <section>
+        <h1>My Profile</h1>
+        <div class="tab">
+            <button class="tablinks" onclick="openTab(event, 'Info')">Basic Information</button>
+            <button class="tablinks" onclick="openTab(event, 'Password')">Password</button>
+            <button class="tablinks" onclick="openTab(event, 'History')">Training History</button>
+        </div>
+
+        <div id="Info" class="tabcontent">
+            <p>Information from registration form.</p>
+        </div>
+
+        <div id="Password" class="tabcontent">
+            <p>allow changing of password</p> 
+        </div>
+
+        <div id="History" class="tabcontent">
+            <p>Display training History</p>
+        </div>
+    </section>
+    <div>
+        <!--chat icon-->
+    </div>
+  <?php include "footer.php"; ?>
+  
+</body>
+  
+</html>

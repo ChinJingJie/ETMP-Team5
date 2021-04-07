7cdf2553c441eec6a20a04c7dc63beac330426f4
CHIN JING JIE <kocekwong@gmail.com>
2021-04-07 21:00:51 +0800

    basic view of 2 course selection pages

---
+++ ETMP/trainingcourse.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include "header.php";?>
+<body>
+    <header>
+        <?php include "navigation.php";?>
+    </header>
+    <section>
+        <h1>Training Course</h1>
+        <div class="accordion1">
+            <div class="progress-tab1">
+                <input type="checkbox" id="course1"/>
+                <label class="progress-label1" for="course1"><img src="images/couse_leadership.jpeg" alt="logo"/>Build Your Confidence</label>
+                <div class="tab-content1">
+                    <p>Details of the course</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <div class="sticky">
+        <a href="#">
+	       <i class="fa fa-comment"></i>
+        </a>
+    </div>
+  <?php include "footer.php"; ?>
+  
+</body>
+  
+</html>

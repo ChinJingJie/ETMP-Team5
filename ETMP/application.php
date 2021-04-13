8fdc47adc15a074403acf8f0a354a055f679db80
CHIN JING JIE <kocekwong@gmail.com>
2021-04-14 00:18:45 +0800

    update css media query

--- ETMP/application.php
+++ ETMP/application.php
@@ -13,11 +13,11 @@
         <legend>Details of Person In Charge</legend>
           <p>
 		    <label for="picName">Full Name:</label>
-			<input type="text" id="picName" name="picName" placeholder="PIC name" maxlength="25"/>
+			<input type="text" id="picName" name="picName" placeholder="Profile name" maxlength="25"/>
 		  </p>
           <p>
 		    <label for="email">Email:</label>
-			<input type="text" id="email" name="email" placeholder="Your email"/>
+			<input type="text" id="email" name="email" placeholder="KennyOmega29@Gmail.com"/>
 		  </p>
           <p>
 		    <label for="phone">Phone Number:</label>
@@ -29,25 +29,25 @@
 		<legend>Training Venue</legend>
           <p>
 		    <label for="venue">Venue Name:</label>
-			<input type="text" id="venue" name="venue" maxlength="40"/>
+			<input type="text" id="venue" name="venue" placeholder="Sova Hall" maxlength="40"/>
 		  </p>
           <p>
 		    <label for="street">Street Address:</label>
-			<input type="text" id="street" name="address" maxlength="40"/>
+			<input type="text" id="street" name="address" placeholder="Jalan Sesame Street" maxlength="40"/>
 		  </p>
           <p>
 		    <label for="city">City/Town:</label>
-			<input type="text" id="city" name="city" maxlength="20"/>
+			<input type="text" id="city" name="city" placeholder="Kuching" maxlength="20"/>
 		  </p>
           <p>
 		    <label for="code">Postcode:</label>
-			<input type="text" id="code" name="postcode" maxlength="5"/>
+			<input type="text" id="code" name="postcode" placeholder="93300" maxlength="5"/>
 		  </p>           
       </fieldset>
         
       <fieldset>   
         <legend>Training Course</legend>
-        <p><label>Taining Program:</label>
+        <p><label>Training Program:</label>
           <select name="products" id="product">
               <option>Training 1</option>
               <option>Training 2</option>
@@ -55,24 +55,28 @@
           </select>
         </p>
         <p><label>Category:</label>
-          <input type="text" id="category" readonly/>
+          <input type="text" id="category" placeholder="Sports" readonly/>
         </p>
         <p>
-		  <label for="days">Training Date:</label>
-		  <input type="text" id="days" name="days" min="0" max="10" value=""/>
+		  <label for="days">Training Start Date:</label>
+		  <input type="date" id="days" name="days"/>
+          <label for="days">End Date:</label>
+		  <input type="date" id="days" name="days"/>
 		</p>
         <p>
-		  <label for="time">Training Time:</label>
-		  <input type="text" id="time" name="time"/>
+		  <label for="time">Training Start Time:</label>
+		  <input type="time" id="time" name="time"/>
+          <label for="time">End Time:</label>
+		  <input type="time" id="time" name="time"/>
 		</p>
         <p>
-		  <label for="template">Training Template Upload:</label>
+		  <label for="template">Training Template:</label>
 		  <input type="file" id="template" name="template"/>
 		</p>
         <p>
 		  <label class="comment" for="comment">Remarks</label>
 		  <br/>
-		  <textarea id="comment" name="comment" rows="4" cols="20" placeholder="Enter remarks if any"></textarea>
+		  <textarea id="comment" name="comment" rows="4" cols="50" placeholder="Enter remarks if any"></textarea>
 		</p>           
       </fieldset>   
       <div class="button">

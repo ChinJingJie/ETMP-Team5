0896f7d29cad9e761c2b64cf32d953f0fa6670d9
George Chan <pillowbeargeorge@gmail.com>
2021-04-14 16:15:31 +0800

    delete duplicated codes

--- ETMP/errors.php
+++ ETMP/errors.php
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
-<?php if (count($errors) > 0): ?>
-	<div class ="error">
-		<?php foreach ($errors as $error): ?>
-			<p><?php echo $error; ?></p>
-		<?php endforeach ?>
-	</div>
-=======
 <?php if (count($errors) > 0): ?>
 	<div class ="error">
 		<?php foreach ($errors as $error): ?>
 			<p style="color: red;"><?php echo $error; ?></p>
 		<?php endforeach ?>
 	</div>
->>>>>>> ca0545b31f0535b68d2a3c59779c48ba1f9a9a9b
 <?php endif ?>
